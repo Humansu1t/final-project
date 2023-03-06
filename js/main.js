3819191d4851e60d3e6c045ca55eed5b6fb21137
Amirkhon Abdullaev <94994368+Humansu1t@users.noreply.github.com>
2023-03-07 01:46:13 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,5 +6,13 @@ $(function(){
         nextArrow: '<button class="banner-slider-btn banner-slider-btnnext"><img src="images/right.svg" alt=""></button>',
     });
    
+     $('.tabs-item').on('click', function(e){
+        e.preventDefault();
+
+        $('.tabs-item').removeClass('tab-active');
+        $('.search-content-item').removeClass('search-active');
+        $(this).addClass('tab-active');
+        $($(this).attr('href')).addClass('search-active')
+    });
 
 });
