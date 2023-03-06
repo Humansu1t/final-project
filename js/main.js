589769e9a61524dbd7ba2509a6962dc783a8234e
Amirkhon Abdullaev <94994368+Humansu1t@users.noreply.github.com>
2023-03-06 13:50:12 +0300

    slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    $('.banner-slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-slider-btn banner-slider-btnprev"><img src="images/left.svg" alt=""></button>',
+        nextArrow: '<button class="banner-slider-btn banner-slider-btnnext"><img src="images/right.svg" alt=""></button>',
+    });
+   
+
+});
