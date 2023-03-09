26901847c3d644aa7bde43c19635867c1b8b25b8
Amirkhon Abdullaev <94994368+Humansu1t@users.noreply.github.com>
2023-03-09 23:52:10 +0300

    new slick&tab

--- js/main.js
+++ js/main.js
@@ -6,13 +6,27 @@ $(function(){
         nextArrow: '<button class="banner-slider-btn banner-slider-btnnext"><img src="images/right.svg" alt=""></button>',
     });
    
-     $('.tabs-item').on('click', function(e){
+    $('.tab').on('click', function(e){
         e.preventDefault();
 
-        $('.tabs-item').removeClass('tab-active');
-        $('.search-content-item').removeClass('search-active');
+        $('.tab').removeClass('tab-active');
+        $('.tabs-content').removeClass('tabs-content-active');
         $(this).addClass('tab-active');
-        $($(this).attr('href')).addClass('search-active')
+        $($(this).attr('href')).addClass('tabs-content-active')
     });
+
+
+    $('.favs').on('click', function(){
+        $(this).toggleClass('favs-active')
+
+    });
+
+    $('.product-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider-btn product-slider-btnprev"><img src="images/left-black.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider-btn product-slider-btnnext"><img src="images/right-black.svg" alt=""></button>',
+    });
+
 
 });
